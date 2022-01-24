8f057487b6c5063138248e43d51f879460070c61
yenuri <yenuri.yz.28@gmail.com>
2022-01-24 08:29:30 -0500

    config inicial

---
+++ src/test/java/com/bootcamp/credit/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
